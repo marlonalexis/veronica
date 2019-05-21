5c59c961f05db88b02fabd2a00aec85719788743
Rolando <rolando.roc@gmail.com>
2019-05-21 15:34:34 -0500

    Add database creation script

--- veronica-app/postgres/veronica_db.sh
+++ veronica-app/postgres/veronica_db.sh
@@ -1,14 +1,4 @@
-sudo printf "CREATE DATABASE veronica-db;" > veronica_db.sql
-
-sudo -U postgres psql -f veronica_db.sql
-
 echo "Running veronica_schema.sql"
-sudo -U postgres psql -U postgres veronica-db < /tmp/veronica_schema.sql
-
-
+psql -U postgres -f /tmp/veronica_schema.sql
 echo "Running veronica_data.sql"
-sudo -U postgres psql -U postgres veronica-db < /tmp/veronica_data.sql
-
-echo "Finished Database section"
-
-exit
+psql -U postgres -f /tmp/veronica_data.sql
