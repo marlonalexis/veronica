ab43c3cd7a926e35b17104b51b4db302e2b583a9
Rolando <rolando.roc@gmail.com>
2019-05-22 11:56:19 -0500

    Reafctor Fabric8 plugin

--- src/scripts/BuildDockerfile.groovy
+++ src/scripts/BuildDockerfile.groovy
@@ -3,11 +3,11 @@ String template = new File("${project.basedir}/src/docker/DockerfileTemplate".to
 def dockerFileText = new groovy.text.SimpleTemplateEngine().createTemplate(template)
         .make([fileName: project.build.finalName])
 
-println "writing dir " + "${project.basedir}/target/dist"
-new File("${project.basedir}/target/dist/".toString()).mkdirs()
+println "writing dir " + "${project.basedir}/target"
+new File("${project.basedir}/target/".toString()).mkdirs()
 
 println "writing file"
-File dockerFile = new File("${project.basedir}/target/dist/Dockerfile".toString())
+File dockerFile = new File("${project.basedir}/target/Dockerfile".toString())
 
 dockerFile.withWriter('UTF-8') { writer ->
     writer.write(dockerFileText)
